d365a36b6b1b4e61992d97761144c120eceb1754
Cadu Higueras <cadu.higueras@gmail.com>
2023-11-02 09:43:59 +0100

    feature/3-common-elements - Refactor import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Providers } from "@/lib/providers";
 import Sidebar from "@/components/sidebar/sidebar";
-import Header from "@/components/header";
+import Header from "@/components/header/header";
 
 export default function RootLayout(  props: React.PropsWithChildren ) {
   return (
