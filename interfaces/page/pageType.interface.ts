bb86ed8cf508e650ba9f29dea0a350341ffebf61
Cadu Higueras <cadu.higueras@gmail.com>
2023-11-03 11:09:05 +0100

    feature/4-pages-components - Separating interfaces in different folder and adding interfaces for page type

---
+++ interfaces/page/pageType.interface.ts
@@ -0,0 +1,21 @@
+export enum allowedPageTypes {
+    text ='text',
+    textarea ='textarea',
+    wysiwyg ='wysiwyg',
+    select ='select',
+    multiselect ='multiselect',
+    checkbox ='checkbox',
+    radio ='radio',
+    repeater ='repeater',
+    image = 'image',
+    fileUploader = 'fileUploader',
+}
+export interface PageTypeInterface {
+    id: number,
+    name: string,
+    label: string,
+    type: allowedPageTypes,
+    instructions: string,
+    settings: object, //todo own interface,
+    color: string,
+}
