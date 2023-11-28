00c647ca359c9d8047317742934775b4b016a832
Cadu Higueras <cadu.higueras@gmail.com>
2023-11-28 20:15:18 +0100

    feature/6-all-pages-lists-and-actions - (WIP) Adding filters and buttons components

---
+++ components/page/list/filters.tsx
@@ -0,0 +1,7 @@
+export function Filters({setIsFiltersOpen, isFiltersOpen}: any) {
+    return (
+        <div className={`transition-all w-full ${isFiltersOpen ? 'h-[100px]' : 'h-0 overflow-hidden' }`}>
+            test
+        </div>
+    )
+}
